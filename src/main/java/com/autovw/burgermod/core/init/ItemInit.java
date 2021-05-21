1191c36e77009de96d5017751c594cd08d28d106
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-21 11:51:38 +0200

    Added pork & beef burger, chicken nugget, tweaking
    Made original Burger no longer show up in Creative menu.
    Added beef & pork burger.
    Tweaked scrambled egg. (shortened poison effect from 12 to 8 seconds.)
    Tweaked raw scrambled egg's saturation.
    Added cooked chicken nugget.

--- src/main/java/com/autovw/burgermod/core/init/ItemInit.java
+++ src/main/java/com/autovw/burgermod/core/init/ItemInit.java
@@ -14,17 +14,30 @@ public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BurgerMod.MOD_ID);
 
+    // Burgers
     public static final RegistryObject<Item> BURGER = ITEMS.register("burger",
+            () -> new Item(new Item.Properties().food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
+
+    public static final RegistryObject<Item> BEEF_BURGER = ITEMS.register("beef_burger",
             () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
                     .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
 
+    public static final RegistryObject<Item> PORK_BURGER = ITEMS.register("pork_burger",
+            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
+                    .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
+
+    // Eggs
     public static final RegistryObject<Item> SCRAMBLED_EGG = ITEMS.register("scrambled_egg",
             () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
                     .food(new Food.Builder().hunger(1).saturation(0.1f).effect(() -> new EffectInstance
-                            (Effects.POISON, 240, 0), 1.0f).build())));
+                            (Effects.POISON, 160, 0), 1.0f).build())));
 
     public static final RegistryObject<Item> FRIED_SCRAMBLED_EGG = ITEMS.register("fried_scrambled_egg",
             () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(2).saturation(0.5f).build())));
+                    .food(new Food.Builder().hunger(2).saturation(0.6f).build())));
 
+    // Others
+    public static final RegistryObject<Item> COOKED_CHICKEN_NUGGET = ITEMS.register("cooked_chicken_nugget",
+            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
+                    .food(new Food.Builder().hunger(1).saturation(0.6f).meat().build())));
 }
