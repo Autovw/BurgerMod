308315b8a7fd020048fc36b1a2cba00c69247b48
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-23 15:38:44 +0200

    util

---
+++ src/main/java/com/autovw/burgermod/util/IHasModel.java
@@ -0,0 +1,6 @@
+package com.autovw.burgermod.util;
+
+public interface IHasModel {
+
+    public void registerModels();
+}
---
+++ src/main/java/com/autovw/burgermod/util/handlers/RegistryHandler.java
@@ -0,0 +1,27 @@
+package com.autovw.burgermod.util.handlers;
+
+import com.autovw.burgermod.core.init.ItemInit;
+import com.autovw.burgermod.util.IHasModel;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class RegistryHandler {
+
+    @SubscribeEvent
+    public static void registerItem(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        for(Item item : ItemInit.ITEMS) {
+            if(item instanceof IHasModel) {
+                ((IHasModel)item).registerModels();
+            }
+        }
+    }
+}
