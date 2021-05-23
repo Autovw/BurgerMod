14bca5a12b4148b8c076acce1c393cb126527177
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-23 15:37:22 +0200

    Update ItemInit.java

--- src/main/java/com/autovw/burgermod/core/init/ItemInit.java
+++ src/main/java/com/autovw/burgermod/core/init/ItemInit.java
@@ -1,64 +1,31 @@
 package com.autovw.burgermod.core.init;
 
-import com.autovw.burgermod.BurgerMod;
-import net.minecraft.item.Food;
+import com.autovw.burgermod.item.items.ItemBase;
+import com.autovw.burgermod.item.items.ItemRaw;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
-import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemInit {
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BurgerMod.MOD_ID);
+    public static final List<Item> ITEMS = new ArrayList<Item>();
 
-    // Meat + Egg Burgers
-    public static final RegistryObject<Item> BURGER = ITEMS.register("burger",
-            () -> new Item(new Item.Properties().food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
+    // Egg Meat Burgers
+    public static final Item BEEF_BURGER = new ItemBase("beef_burger", 10, 1.0f, true);
+    public static final Item PORK_BURGER = new ItemBase("pork_burger", 10, 1.0f, true);
+    public static final Item MUTTON_BURGER = new ItemBase("mutton_burger", 8, 1.0f, true);
 
-    public static final RegistryObject<Item> BEEF_BURGER = ITEMS.register("beef_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
-
-    public static final RegistryObject<Item> PORK_BURGER = ITEMS.register("pork_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
-
-    public static final RegistryObject<Item> MUTTON_BURGER = ITEMS.register("mutton_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(8).meat().saturation(1.0f).build())));
-
-    // Meat + Cheese Burgers
-    public static final RegistryObject<Item> BEEF_CHEESE_BURGER = ITEMS.register("beef_cheese_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
-
-    public static final RegistryObject<Item> PORK_CHEESE_BURGER = ITEMS.register("pork_cheese_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(10).meat().saturation(1.0f).build())));
-
-    public static final RegistryObject<Item> MUTTON_CHEESE_BURGER = ITEMS.register("mutton_cheese_burger",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(8).meat().saturation(1.0f).build())));
+    // Cheese Meat Burgers
+    public static final Item BEEF_CHEESE_BURGER = new ItemBase("beef_cheese_burger", 10, 1.0f, true);
+    public static final Item PORK_CHEESE_BURGER = new ItemBase("pork_cheese_burger", 10, 1.0f, true);
+    public static final Item MUTTON_CHEESE_BURGER = new ItemBase("mutton_cheese_burger", 8, 1.0f, true);
 
     // Eggs
-    public static final RegistryObject<Item> SCRAMBLED_EGG = ITEMS.register("scrambled_egg",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(1).saturation(0.1f).effect(() -> new EffectInstance
-                            (Effects.POISON, 160, 0), 1.0f).build())));
-
-    public static final RegistryObject<Item> FRIED_SCRAMBLED_EGG = ITEMS.register("fried_scrambled_egg",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(2).meat().saturation(0.6f).build())));
+    public static final Item SCRAMBLED_EGG = new ItemRaw("scrambled_egg", 1, 0.1f, false).addPoisonEffect();
+    public static final Item FRIED_SCRAMBLED_EGG = new ItemBase("fried_scrambled_egg", 2, 0.6f, true);
 
     // Others
-    public static final RegistryObject<Item> COOKED_CHICKEN_NUGGET = ITEMS.register("cooked_chicken_nugget",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(1).saturation(0.6f).meat().build())));
-
-    public static final RegistryObject<Item> CHEESE = ITEMS.register("cheese",
-            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)
-                    .food(new Food.Builder().hunger(2).saturation(0.6f).build())));
+    public static final Item COOKED_CHICKEN_NUGGET = new ItemBase("cooked_chicken_nugget", 1, 0.6f, true);
+    public static final Item CHEESE = new ItemBase("cheese", 2, 0.6f, false);
 }
