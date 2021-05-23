f046974bf15b33b44bc756857d7d03e6ab2866c1
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-23 15:38:26 +0200

    proxy

---
+++ src/main/java/com/autovw/burgermod/proxy/ClientProxy.java
@@ -0,0 +1,28 @@
+package com.autovw.burgermod.proxy;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ClientProxy implements IProxy {
+
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    @Override
+    public void registerModel(Item item, int metadata) {
+        ModelLoader.setCustomModelResourceLocation(item, metadata, new ModelResourceLocation(item.getRegistryName(), "inventory"));
+    }
+}
---
+++ src/main/java/com/autovw/burgermod/proxy/ServerProxy.java
@@ -0,0 +1,25 @@
+package com.autovw.burgermod.proxy;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class ServerProxy implements IProxy {
+
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+
+    public void registerModel(Item item, int metadata) {
+
+    }
+}
