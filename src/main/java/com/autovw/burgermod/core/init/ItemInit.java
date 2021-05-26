0d2aef4b1a3b71af9783fef08ff763144deefd30
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-26 14:43:13 +0200

    Added 2 new items

--- src/main/java/com/autovw/burgermod/core/init/ItemInit.java
+++ src/main/java/com/autovw/burgermod/core/init/ItemInit.java
@@ -15,11 +15,13 @@ public class ItemInit {
     public static final Item BEEF_BURGER = new ItemBase("beef_burger", 10, 1.0f, true);
     public static final Item PORK_BURGER = new ItemBase("pork_burger", 10, 1.0f, true);
     public static final Item MUTTON_BURGER = new ItemBase("mutton_burger", 8, 1.0f, true);
+    public static final Item CHICKEN_BURGER = new ItemBase("chicken_burger", 8, 0.8f, true);
 
     // Cheese Meat Burgers
     public static final Item BEEF_CHEESE_BURGER = new ItemBase("beef_cheese_burger", 10, 1.0f, true);
     public static final Item PORK_CHEESE_BURGER = new ItemBase("pork_cheese_burger", 10, 1.0f, true);
     public static final Item MUTTON_CHEESE_BURGER = new ItemBase("mutton_cheese_burger", 8, 1.0f, true);
+    public static final Item CHICKEN_CHEESE_BURGER = new ItemBase("chicken_cheese_burger", 8, 0.8f, true);
 
     // Eggs
     public static final Item SCRAMBLED_EGG = new ItemRaw("scrambled_egg", 1, 0.1f, false).addPoisonEffect();
