d19388d9da9e989c3a544f4d242503ff065b9785
Autovw <68228006+Autovw@users.noreply.github.com>
2021-05-30 14:20:48 +0200

    Update BurgerMod.java

--- src/main/java/com/autovw/burgermod/BurgerMod.java
+++ src/main/java/com/autovw/burgermod/BurgerMod.java
@@ -16,7 +16,7 @@ public class BurgerMod
 {
     public static final String MODID = "burgermod";
     public static final String NAME = "Burger Mod";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.5.0";
 
     private static Logger logger;
 
