cbbfcf6f7e84015544b3329de3e332d6aa4b9106
Autovw <68228006+Autovw@users.noreply.github.com>
2021-07-07 12:57:46 +0200

    Updated main class to support config

--- src/main/java/com/autovw/burgermod/BurgerMod.java
+++ src/main/java/com/autovw/burgermod/BurgerMod.java
@@ -1,10 +1,13 @@
 package com.autovw.burgermod;
 
+import com.autovw.burgermod.config.Config;
 import com.autovw.burgermod.core.ModItems;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
@@ -20,6 +23,8 @@ public class BurgerMod {
     public static final ItemGroup TAB_BURGERMOD = new BurgerGroup("burgermod_group");
 
     public BurgerMod() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.commonConfig);
+
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::setup);
 
