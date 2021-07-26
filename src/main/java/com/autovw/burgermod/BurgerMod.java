099adca8835bd90896ab43b7c7a20a8eb9822149
Autovw <68228006+Autovw@users.noreply.github.com>
2021-07-26 13:22:04 +0200

    ⬆️ Updated mod version.

--- src/main/java/com/autovw/burgermod/BurgerMod.java
+++ src/main/java/com/autovw/burgermod/BurgerMod.java
@@ -11,12 +11,14 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Author: Autovw
+ */
 @Mod(modid = com.autovw.burgermod.BurgerMod.MODID, name = com.autovw.burgermod.BurgerMod.NAME, version = com.autovw.burgermod.BurgerMod.VERSION)
-public class BurgerMod
-{
+public class BurgerMod {
     public static final String MODID = "burgermod";
     public static final String NAME = "Burger Mod";
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.7.0";
 
     private static Logger logger;
 
