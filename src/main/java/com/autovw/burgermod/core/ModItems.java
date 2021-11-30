0804a9a7c275351c6a08f098b29131f86e6822a2
Autovw <68228006+Autovw@users.noreply.github.com>
2021-11-30 19:53:11 +0100

    ⬆️ Updated to Forge 1.18

--- src/main/java/com/autovw/burgermod/core/ModItems.java
+++ src/main/java/com/autovw/burgermod/core/ModItems.java
@@ -4,9 +4,9 @@ import com.autovw.burgermod.BurgerMod;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
-import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
  * Author: Autovw
