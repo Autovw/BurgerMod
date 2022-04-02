9fbfdc608e983b004d2b27f9c83455fc91456a81
Autovw <68228006+Autovw@users.noreply.github.com>
2022-04-02 14:13:06 +0200

    ✨ Items from mod are now displayed first in the creative tab

--- src/main/java/com/autovw/burgermod/BurgerCreativeTab.java
+++ src/main/java/com/autovw/burgermod/BurgerCreativeTab.java
@@ -5,6 +5,8 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.Objects;
+
 /**
  * Author: Autovw
  */
@@ -23,5 +25,13 @@ public class BurgerCreativeTab extends CreativeModeTab {
     @Override
     public void fillItemList(NonNullList<ItemStack> items) {
         super.fillItemList(items);
+        items.sort((s1, s2) -> {
+            String item1 = Objects.requireNonNull(s1.getItem().getRegistryName()).getNamespace();
+            String item2 = Objects.requireNonNull(s2.getItem().getRegistryName()).getNamespace();
+
+            if (item1.equals(BurgerMod.MOD_ID) == item2.equals(BurgerMod.MOD_ID)) return 0;
+
+            return item1.equals(BurgerMod.MOD_ID) ? -1 : 1;
+        });
     }
 }
