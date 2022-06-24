16ea16d63cf8bc064875da4aa77d6658658f4e19
Autovw <68228006+Autovw@users.noreply.github.com>
2022-06-24 15:30:17 +0200

    ⚡ Increased chance of Enchanted Golden Burger generating

--- src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
+++ src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
@@ -38,7 +38,7 @@ public class ModLootModifierProvider extends GlobalLootModifierProvider {
         addRangedChance(BurgerMod.MOD_ID, new ResourceLocation("chests/ruined_portal"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.15f);
         addRangedChance(BurgerMod.MOD_ID, new ResourceLocation("chests/bastion_other"), ModItems.GOLDEN_SALMON_BURGER.get(), 0, 1, 0.2f);
         addRangedChance(BurgerMod.MOD_ID, new ResourceLocation("chests/bastion_treasure"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.4f);
-        addRangedChance(BurgerMod.MOD_ID, new ResourceLocation("chests/bastion_treasure"), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.15f);
+        addRangedChance(BurgerMod.MOD_ID, new ResourceLocation("chests/bastion_treasure"), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.2f);
     }
 
     // helper method for generating a new RangedAdditionModifier
