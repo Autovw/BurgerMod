43ec1d25d2fe2a0136691c297d6aceca2a9ea435
Autovw <68228006+Autovw@users.noreply.github.com>
2022-06-24 15:22:02 +0200

    🐛 Fixed loot modifiers not injecting loot

--- src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
+++ src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
@@ -23,34 +23,34 @@ public class ModLootModifierProvider extends GlobalLootModifierProvider {
 
     @Override
     protected void start() {
-        addRanged(ModLootModifiers.VILLAGE_PLAINS_HOUSE_FRIES_ADDITION.get(), ModItems.FRIES.get(), 0, 2);
-        addRanged(ModLootModifiers.VILLAGE_SNOWY_HOUSE_HOTDOG_ADDITION.get(), ModItems.HOTDOG.get(), 0, 3);
-        addRanged(ModLootModifiers.VILLAGE_BUTCHER_COOKED_CHICKEN_NUGGET_ADDITION.get(), ModItems.COOKED_CHICKEN_NUGGET.get(), 2, 5);
-        addRanged(ModLootModifiers.VILLAGE_FISHER_COD_BURGER_ADDITION.get(), ModItems.COD_BURGER.get(), 0, 1);
-        addRanged(ModLootModifiers.VILLAGE_FISHER_SALMON_BURGER_ADDITION.get(), ModItems.SALMON_BURGER.get(), 0, 1);
-        addRanged(ModLootModifiers.IGLOO_CHEST_PORK_CHEESE_BURGER_ADDITION.get(), ModItems.PORK_CHEESE_BURGER.get(), 0, 1);
-        addRanged(ModLootModifiers.SIMPLE_DUNGEON_SCRAMBLED_EGG_ADDITION.get(), ModItems.SCRAMBLED_EGG.get(), 1, 2);
-        addRanged(ModLootModifiers.WOODLAND_MANSION_COOKED_CHAMPIGNONS_ADDITION.get(), ModItems.COOKED_CHAMPIGNONS.get(), 0, 4);
-        addRanged(ModLootModifiers.WOODLAND_MANSION_MUTTON_CHAMPIGNON_BURGER_ADDITION.get(), ModItems.MUTTON_CHAMPIGNON_BURGER.get(), 0, 1);
+        addRanged(ModLootModifiers.VILLAGE_PLAINS_HOUSE_FRIES_ADDITION.get(), new ResourceLocation("chests/village/village_plains_house"), ModItems.FRIES.get(), 0, 2);
+        addRanged(ModLootModifiers.VILLAGE_SNOWY_HOUSE_HOTDOG_ADDITION.get(), new ResourceLocation("chests/village/village_snowy_house"), ModItems.HOTDOG.get(), 0, 3);
+        addRanged(ModLootModifiers.VILLAGE_BUTCHER_COOKED_CHICKEN_NUGGET_ADDITION.get(), new ResourceLocation("chests/village/village_butcher"), ModItems.COOKED_CHICKEN_NUGGET.get(), 2, 5);
+        addRanged(ModLootModifiers.VILLAGE_FISHER_COD_BURGER_ADDITION.get(), new ResourceLocation("chests/village/village_fisher"), ModItems.COD_BURGER.get(), 0, 1);
+        addRanged(ModLootModifiers.VILLAGE_FISHER_SALMON_BURGER_ADDITION.get(), new ResourceLocation("chests/village/village_fisher"), ModItems.SALMON_BURGER.get(), 0, 1);
+        addRanged(ModLootModifiers.IGLOO_CHEST_PORK_CHEESE_BURGER_ADDITION.get(), new ResourceLocation("chests/igloo_chest"), ModItems.PORK_CHEESE_BURGER.get(), 0, 1);
+        addRanged(ModLootModifiers.SIMPLE_DUNGEON_SCRAMBLED_EGG_ADDITION.get(), new ResourceLocation("chests/simple_dungeon"), ModItems.SCRAMBLED_EGG.get(), 1, 2);
+        addRanged(ModLootModifiers.WOODLAND_MANSION_COOKED_CHAMPIGNONS_ADDITION.get(), new ResourceLocation("chests/woodland_mansion"), ModItems.COOKED_CHAMPIGNONS.get(), 0, 4);
+        addRanged(ModLootModifiers.WOODLAND_MANSION_MUTTON_CHAMPIGNON_BURGER_ADDITION.get(), new ResourceLocation("chests/woodland_mansion"), ModItems.MUTTON_CHAMPIGNON_BURGER.get(), 0, 1);
 
-        addRangedChance(ModLootModifiers.SIMPLE_DUNGEON_GOLDEN_BEEF_BURGER_ADDITION.get(), ModItems.GOLDEN_BEEF_BURGER.get(), 1, 1, 0.25f);
-        addRangedChance(ModLootModifiers.DESERT_PYRAMID_GOLDEN_MUTTON_BURGER_ADDITION.get(), ModItems.GOLDEN_MUTTON_BURGER.get(), 1, 1, 0.1f);
-        addRangedChance(ModLootModifiers.RUINED_PORTAL_GOLDEN_PORK_BURGER_ADDITION.get(), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.15f);
-        addRangedChance(ModLootModifiers.BASTION_OTHER_GOLDEN_SALMON_BURGER_ADDITION.get(), ModItems.GOLDEN_SALMON_BURGER.get(), 0, 1, 0.2f);
-        addRangedChance(ModLootModifiers.BASTION_TREASURE_GOLDEN_PORK_BURGER_ADDITION.get(), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.4f);
-        addRangedChance(ModLootModifiers.BASTION_TREASURE_ENCHANTED_GOLDEN_BURGER_ADDITION.get(), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.15f);
+        addRangedChance(ModLootModifiers.SIMPLE_DUNGEON_GOLDEN_BEEF_BURGER_ADDITION.get(), new ResourceLocation("chests/simple_dungeon"), ModItems.GOLDEN_BEEF_BURGER.get(), 1, 1, 0.25f);
+        addRangedChance(ModLootModifiers.DESERT_PYRAMID_GOLDEN_MUTTON_BURGER_ADDITION.get(), new ResourceLocation("chests/desert_pyramid"), ModItems.GOLDEN_MUTTON_BURGER.get(), 1, 1, 0.1f);
+        addRangedChance(ModLootModifiers.RUINED_PORTAL_GOLDEN_PORK_BURGER_ADDITION.get(), new ResourceLocation("chests/ruined_portal"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.15f);
+        addRangedChance(ModLootModifiers.BASTION_OTHER_GOLDEN_SALMON_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_other"), ModItems.GOLDEN_SALMON_BURGER.get(), 0, 1, 0.2f);
+        addRangedChance(ModLootModifiers.BASTION_TREASURE_GOLDEN_PORK_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_treasure"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.4f);
+        addRangedChance(ModLootModifiers.BASTION_TREASURE_ENCHANTED_GOLDEN_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_treasure"), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.15f);
     }
 
     // helper method for generating a new RangedAdditionModifier
-    protected void addRanged(RangedAdditionModifier.Serializer serializer, Item addition, int minAddition, int maxAddition) {
+    protected void addRanged(RangedAdditionModifier.Serializer serializer, ResourceLocation target, Item addition, int minAddition, int maxAddition) {
         ResourceLocation lootLoc = ForgeRegistries.LOOT_MODIFIER_SERIALIZERS.get().getKey(serializer);
         add(lootLoc.getPath(), serializer, new RangedAdditionModifier(new LootItemCondition[] {
-                LootTableIdCondition.builder(lootLoc).build() }, addition, minAddition, maxAddition));
+                LootTableIdCondition.builder(target).build() }, addition, minAddition, maxAddition));
     }
 
-    protected void addRangedChance(RangedChanceAdditionModifier.Serializer serializer, Item addition, int minAddition, int maxAddition, float chanceAddition) {
+    protected void addRangedChance(RangedChanceAdditionModifier.Serializer serializer, ResourceLocation target, Item addition, int minAddition, int maxAddition, float chanceAddition) {
         ResourceLocation lootLoc = ForgeRegistries.LOOT_MODIFIER_SERIALIZERS.get().getKey(serializer);
         add(lootLoc.getPath(), serializer, new RangedChanceAdditionModifier(new LootItemCondition[] {
-                LootTableIdCondition.builder(lootLoc).build() }, addition, minAddition, maxAddition, chanceAddition));
+                LootTableIdCondition.builder(target).build() }, addition, minAddition, maxAddition, chanceAddition));
     }
 }
