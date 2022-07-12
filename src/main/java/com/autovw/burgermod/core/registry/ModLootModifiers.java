ec3a11e0d7cb0d4bf3ef1eb00db91db8139fe73e
Autovw <68228006+Autovw@users.noreply.github.com>
2022-07-12 12:50:08 +0200

    ⚡ Updated loot modifier(s) to new system

--- src/main/java/com/autovw/burgermod/core/registry/ModLootModifiers.java
+++ src/main/java/com/autovw/burgermod/core/registry/ModLootModifiers.java
@@ -2,7 +2,8 @@ package com.autovw.burgermod.core.registry;
 
 import com.autovw.burgermod.BurgerMod;
 import com.autovw.burgermod.common.loot.LootAdditionModifier;
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import com.mojang.serialization.Codec;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,24 +12,7 @@ import net.minecraftforge.registries.RegistryObject;
  * Author: Autovw
  */
 public class ModLootModifiers {
-    public static final DeferredRegister<GlobalLootModifierSerializer<?>> LOOT_MODIFIERS = DeferredRegister.create(ForgeRegistries.Keys.LOOT_MODIFIER_SERIALIZERS, BurgerMod.MOD_ID);
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIERS = DeferredRegister.create(ForgeRegistries.Keys.LOOT_MODIFIER_SERIALIZERS, BurgerMod.MOD_ID);
 
-    public static final RegistryObject<LootAdditionModifier.Serializer> VILLAGE_PLAINS_HOUSE_FRIES_ADDITION = LOOT_MODIFIERS.register("chests/village/village_plains_house/fries_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> VILLAGE_SNOWY_HOUSE_HOTDOG_ADDITION = LOOT_MODIFIERS.register("chests/village/village_snowy_house/hotdog_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> VILLAGE_BUTCHER_COOKED_CHICKEN_NUGGET_ADDITION = LOOT_MODIFIERS.register("chests/village/village_butcher/cooked_chicken_nugget_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> VILLAGE_FISHER_COD_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/village/village_fisher/cod_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> VILLAGE_FISHER_SALMON_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/village/village_fisher/salmon_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> IGLOO_CHEST_PORK_CHEESE_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/igloo_chest/pork_cheese_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> SIMPLE_DUNGEON_SCRAMBLED_EGG_ADDITION = LOOT_MODIFIERS.register("chests/simple_dungeon/scrambled_egg_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> WOODLAND_MANSION_COOKED_CHAMPIGNONS_ADDITION = LOOT_MODIFIERS.register("chests/woodland_mansion/cooked_champignons_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> WOODLAND_MANSION_MUTTON_CHAMPIGNON_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/woodland_mansion/mutton_champignon_burger_addition", LootAdditionModifier.Serializer::new);
-
-    public static final RegistryObject<LootAdditionModifier.Serializer> SIMPLE_DUNGEON_GOLDEN_BEEF_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/simple_dungeon/golden_beef_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> DESERT_PYRAMID_GOLDEN_MUTTON_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/desert_pyramid/golden_mutton_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> RUINED_PORTAL_GOLDEN_PORK_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/ruined_portal/golden_pork_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> BASTION_OTHER_GOLDEN_SALMON_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/bastion_other/golden_salmon_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> BASTION_HOGLIN_STABLE_GOLDEN_CHICKEN_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/bastion_hoglin_stable/golden_chicken_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> BASTION_TREASURE_GOLDEN_PORK_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/bastion_treasure/golden_pork_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> BASTION_TREASURE_ENCHANTED_GOLDEN_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/bastion_treasure/enchanted_golden_burger_addition", LootAdditionModifier.Serializer::new);
-    public static final RegistryObject<LootAdditionModifier.Serializer> ANCIENT_CITY_ENCHANTED_GOLDEN_BURGER_ADDITION = LOOT_MODIFIERS.register("chests/ancient_city/enchanted_golden_burger_addition", LootAdditionModifier.Serializer::new);
+    public static final RegistryObject<Codec<LootAdditionModifier>> LOOT_ADDITION_MODIFIER = LOOT_MODIFIERS.register("loot_addition_modifier", () -> LootAdditionModifier.CODEC);
 }
--- src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
+++ src/main/java/com/autovw/burgermod/datagen/providers/ModLootModifierProvider.java
@@ -2,15 +2,9 @@ package com.autovw.burgermod.datagen.providers;
 
 import com.autovw.burgermod.BurgerMod;
 import com.autovw.burgermod.common.loot.LootAdditionModifier;
-import com.autovw.burgermod.core.registry.ModItems;
-import com.autovw.burgermod.core.registry.ModLootModifiers;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.data.GlobalLootModifierProvider;
-import net.minecraftforge.common.loot.LootTableIdCondition;
-import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * Author: Autovw
@@ -22,33 +16,6 @@ public class ModLootModifierProvider extends GlobalLootModifierProvider {
 
     @Override
     protected void start() {
-        addModifier(ModLootModifiers.VILLAGE_PLAINS_HOUSE_FRIES_ADDITION.get(), new ResourceLocation("chests/village/village_plains_house"), ModItems.FRIES.get(), 0, 2);
-        addModifier(ModLootModifiers.VILLAGE_SNOWY_HOUSE_HOTDOG_ADDITION.get(), new ResourceLocation("chests/village/village_snowy_house"), ModItems.HOTDOG.get(), 0, 3);
-        addModifier(ModLootModifiers.VILLAGE_BUTCHER_COOKED_CHICKEN_NUGGET_ADDITION.get(), new ResourceLocation("chests/village/village_butcher"), ModItems.COOKED_CHICKEN_NUGGET.get(), 2, 5);
-        addModifier(ModLootModifiers.VILLAGE_FISHER_COD_BURGER_ADDITION.get(), new ResourceLocation("chests/village/village_fisher"), ModItems.COD_BURGER.get(), 0, 1);
-        addModifier(ModLootModifiers.VILLAGE_FISHER_SALMON_BURGER_ADDITION.get(), new ResourceLocation("chests/village/village_fisher"), ModItems.SALMON_BURGER.get(), 0, 1);
-        addModifier(ModLootModifiers.IGLOO_CHEST_PORK_CHEESE_BURGER_ADDITION.get(), new ResourceLocation("chests/igloo_chest"), ModItems.PORK_CHEESE_BURGER.get(), 0, 1);
-        addModifier(ModLootModifiers.SIMPLE_DUNGEON_SCRAMBLED_EGG_ADDITION.get(), new ResourceLocation("chests/simple_dungeon"), ModItems.SCRAMBLED_EGG.get(), 1, 2);
-        addModifier(ModLootModifiers.WOODLAND_MANSION_COOKED_CHAMPIGNONS_ADDITION.get(), new ResourceLocation("chests/woodland_mansion"), ModItems.COOKED_CHAMPIGNONS.get(), 0, 4);
-        addModifier(ModLootModifiers.WOODLAND_MANSION_MUTTON_CHAMPIGNON_BURGER_ADDITION.get(), new ResourceLocation("chests/woodland_mansion"), ModItems.MUTTON_CHAMPIGNON_BURGER.get(), 0, 1);
-
-        addModifier(ModLootModifiers.SIMPLE_DUNGEON_GOLDEN_BEEF_BURGER_ADDITION.get(), new ResourceLocation("chests/simple_dungeon"), ModItems.GOLDEN_BEEF_BURGER.get(), 1, 1, 0.25f);
-        addModifier(ModLootModifiers.DESERT_PYRAMID_GOLDEN_MUTTON_BURGER_ADDITION.get(), new ResourceLocation("chests/desert_pyramid"), ModItems.GOLDEN_MUTTON_BURGER.get(), 1, 1, 0.1f);
-        addModifier(ModLootModifiers.RUINED_PORTAL_GOLDEN_PORK_BURGER_ADDITION.get(), new ResourceLocation("chests/ruined_portal"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.15f);
-        addModifier(ModLootModifiers.BASTION_OTHER_GOLDEN_SALMON_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_other"), ModItems.GOLDEN_SALMON_BURGER.get(), 0, 1, 0.2f);
-        addModifier(ModLootModifiers.BASTION_HOGLIN_STABLE_GOLDEN_CHICKEN_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_hoglin_stable"), ModItems.GOLDEN_CHICKEN_BURGER.get(), 0, 1, 0.3f);
-        addModifier(ModLootModifiers.BASTION_TREASURE_GOLDEN_PORK_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_treasure"), ModItems.GOLDEN_PORK_BURGER.get(), 1, 1, 0.4f);
-        addModifier(ModLootModifiers.BASTION_TREASURE_ENCHANTED_GOLDEN_BURGER_ADDITION.get(), new ResourceLocation("chests/bastion_treasure"), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.2f);
-        addModifier(ModLootModifiers.ANCIENT_CITY_ENCHANTED_GOLDEN_BURGER_ADDITION.get(), new ResourceLocation("chests/ancient_city"), ModItems.ENCHANTED_GOLDEN_BURGER.get(), 0, 1, 0.25f);
-    }
-
-    public void addModifier(LootAdditionModifier.Serializer serializer, ResourceLocation target, Item addition, int minAddition, int maxAddition) {
-        addModifier(serializer, target, addition, minAddition, maxAddition, 1.0f);
-    }
-
-    public void addModifier(LootAdditionModifier.Serializer serializer, ResourceLocation target, Item addition, int minAddition, int maxAddition, float chanceAddition) {
-        ResourceLocation lootLoc = ForgeRegistries.LOOT_MODIFIER_SERIALIZERS.get().getKey(serializer);
-        add(lootLoc.getPath(), serializer, new LootAdditionModifier(new LootItemCondition[] {
-                LootTableIdCondition.builder(target).build() }, addition, minAddition, maxAddition, chanceAddition));
+        add("loot_addition_modifier", new LootAdditionModifier(new LootItemCondition[] {}));
     }
 }
