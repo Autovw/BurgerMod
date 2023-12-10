d905b6e413ea22bd662e2dd81d651c8b4f7b93a7
Autovw <68228006+Autovw@users.noreply.github.com>
2023-12-10 18:17:13 +0100

    ⬆️ Update mod to Minecraft 1.20.4

--- src/main/java/com/autovw/burgermod/datagen/providers/ModAdvancementProvider.java
+++ src/main/java/com/autovw/burgermod/datagen/providers/ModAdvancementProvider.java
@@ -6,7 +6,7 @@ import com.autovw.burgermod.core.util.ModTags;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementRewards;
-import net.minecraft.advancements.FrameType;
+import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.critereon.*;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
@@ -34,13 +34,13 @@ public class ModAdvancementProvider extends ForgeAdvancementProvider {
         public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> consumer, ExistingFileHelper fileHelper) {
             AdvancementHolder craftBurger = Advancement.Builder.advancement()
                     .parent(new ResourceLocation("husbandry/plant_seed"))
-                    .display(ModItems.BEEF_BURGER.get(), Component.translatable("advancements.burgermod.husbandry.craft_burger.title"), Component.translatable("advancements.burgermod.husbandry.craft_burger.description"), null, FrameType.TASK, true, true, false)
+                    .display(ModItems.BEEF_BURGER.get(), Component.translatable("advancements.burgermod.husbandry.craft_burger.title"), Component.translatable("advancements.burgermod.husbandry.craft_burger.description"), null, AdvancementType.TASK, true, true, false)
                     .addCriterion("burgers", InventoryChangeTrigger.TriggerInstance.hasItems(new ItemPredicate(Optional.of(ModTags.BURGERS), null, MinMaxBounds.Ints.ANY, MinMaxBounds.Ints.ANY, null, null, null, Optional.empty())))
                     .save(consumer, new ResourceLocation(BurgerMod.MOD_ID, "husbandry/craft_burger"));
 
             Advancement.Builder.advancement()
                     .parent(craftBurger)
-                    .display(ModItems.GOLDEN_BEEF_BURGER.get(), Component.translatable("advancements.burgermod.husbandry.craft_golden_burger.title"), Component.translatable("advancements.burgermod.husbandry.craft_golden_burger.description"), null, FrameType.CHALLENGE, true, true, false)
+                    .display(ModItems.GOLDEN_BEEF_BURGER.get(), Component.translatable("advancements.burgermod.husbandry.craft_golden_burger.title"), Component.translatable("advancements.burgermod.husbandry.craft_golden_burger.description"), null, AdvancementType.CHALLENGE, true, true, false)
                     .rewards(AdvancementRewards.Builder.experience(100))
                     .addCriterion("golden_burgers", InventoryChangeTrigger.TriggerInstance.hasItems(new ItemPredicate(Optional.of(ModTags.GOLDEN_BURGERS), null, MinMaxBounds.Ints.ANY, MinMaxBounds.Ints.ANY, null, null, null, Optional.empty())))
                     .save(consumer, new ResourceLocation(BurgerMod.MOD_ID, "husbandry/craft_golden_burger"));
