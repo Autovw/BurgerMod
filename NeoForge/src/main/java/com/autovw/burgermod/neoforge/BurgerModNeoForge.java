339e3378e7693cc3b4cfadff90becf7bc661db09
Autovw <68228006+Autovw@users.noreply.github.com>
2024-12-03 10:30:50 +0100

    ⚡ Use default config screen when Configured is not present (NeoForge)

--- NeoForge/src/main/java/com/autovw/burgermod/neoforge/BurgerModNeoForge.java
+++ NeoForge/src/main/java/com/autovw/burgermod/neoforge/BurgerModNeoForge.java
@@ -1,6 +1,7 @@
 package com.autovw.burgermod.neoforge;
 
 import com.autovw.burgermod.common.BurgerMod;
+import com.autovw.burgermod.neoforge.client.ClientHandler;
 import com.autovw.burgermod.neoforge.config.Config;
 import com.autovw.burgermod.neoforge.core.registry.ModItemRegistry;
 import com.autovw.burgermod.neoforge.core.registry.ModLootModifiers;
@@ -9,6 +10,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import org.slf4j.Logger;
 
@@ -27,6 +29,7 @@ public class BurgerModNeoForge
         container.registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
 
         bus.addListener(this::commonSetup);
+        bus.addListener(this::clientSetup);
 
         ModItemRegistry.ITEMS.register(bus);
         ModLootModifiers.LOOT_MODIFIERS.register(bus);
@@ -35,5 +38,10 @@ public class BurgerModNeoForge
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event)
+    {
+        event.enqueueWork(ClientHandler::onClientSetup);
     }
 }
