53c6d9abd94b2d603e3ffc5ff0c95f9a380dff31
Autovw <68228006+Autovw@users.noreply.github.com>
2024-12-29 13:24:34 +0100

    🐛 Fix Enchanted Golden Burger using Golden Burger regeneration effect

--- Common/src/main/java/com/autovw/burgermod/common/core/util/ModConsumables.java
+++ Common/src/main/java/com/autovw/burgermod/common/core/util/ModConsumables.java
@@ -14,5 +14,5 @@ public final class ModConsumables
     public static final Consumable RAW_SCRAMBLED_EGG = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(ModEffects.RAW_SCRAMBLED_EGG_EFFECTS, 1.0f)).build();
     public static final Consumable RAW_CHAMPIGNONS = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(ModEffects.RAW_CHAMPIGNONS_EFFECTS, 1.0f)).build();
     public static final Consumable GOLDEN_BURGER = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(List.of(ModEffects.GOLDEN_BURGER_REGENERATION, ModEffects.GOLDEN_BURGER_ABSORPTION))).build();
-    public static final Consumable ENCHANTED_GOLDEN_BURGER = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(List.of(ModEffects.GOLDEN_BURGER_REGENERATION, ModEffects.ENCHANTED_GOLDEN_BURGER_DAMAGE_RESISTANCE, ModEffects.ENCHANTED_GOLDEN_BURGER_FIRE_RESISTANCE, ModEffects.ENCHANTED_GOLDEN_BURGER_ABSORPTION))).build();
+    public static final Consumable ENCHANTED_GOLDEN_BURGER = Consumables.defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(List.of(ModEffects.ENCHANTED_GOLDEN_BURGER_REGENERATION, ModEffects.ENCHANTED_GOLDEN_BURGER_DAMAGE_RESISTANCE, ModEffects.ENCHANTED_GOLDEN_BURGER_FIRE_RESISTANCE, ModEffects.ENCHANTED_GOLDEN_BURGER_ABSORPTION))).build();
 }
